c19c62b21f6ae5f0040f0353a4f198ab60ceff7d
rosecollectif-ops <rosecollectif@gmail.com>
2026-09-22 18:21:36 +0100

    Place website files at Pages root

---
+++ site-data.js
@@ -0,0 +1,40 @@
+window.SITE_DATA = {
+  siteName: 'RoseCollectif Minis',
+  tagline: 'Miniature Painting & Commissions',
+  intro: 'A gallery of painted miniatures, projects and commissions.',
+  socials: {
+    instagram: 'https://instagram.com/rosecollectifminis',
+    etsy: '#',
+    facebook: '#'
+  },
+  contact: {
+    formspreeEndpoint: ''
+  },
+  appearance: {
+    background: '#f4f1ec',
+    text: '#202020',
+    accent: '#8d6f5a',
+    card: '#ffffff',
+    heroImage: ''
+  },
+  albums: [
+    {title:'Warhound Titan', category:'Adeptus Titanicus', driveFolder:''},
+    {title:'Horrors of Tzeentch', category:'Warhammer', driveFolder:''},
+    {title:'Saurus Warriors', category:'Warhammer', driveFolder:''},
+    {title:'Kharadron Zontari Endrin Dock', category:'Warhammer', driveFolder:''},
+    {title:'Slann Starmaster', category:'Warhammer', driveFolder:''},
+    {title:'Saurus Oldblood on Carnosaur', category:'Warhammer', driveFolder:''},
+    {title:'Bastiladon (Solar Engine)', category:'Warhammer', driveFolder:''},
+    {title:'Skinks', category:'Warhammer', driveFolder:''},
+    {title:'Skink Starpriest', category:'Warhammer', driveFolder:''},
+    {title:'Succubus', category:'Other', driveFolder:''},
+    {title:'Dragon Dragonkin', category:'Other', driveFolder:''},
+    {title:'Saurus Cold Ones', category:'Warhammer', driveFolder:''},
+    {title:'Miscellaneous', category:'Other', driveFolder:''},
+    {title:'Chameleon Skinks', category:'Warhammer', driveFolder:''},
+    {title:'Salamander Hunting Pack', category:'Warhammer', driveFolder:''},
+    {title:'Terradon Riders', category:'Warhammer', driveFolder:''},
+    {title:'Raptadons', category:'Warhammer', driveFolder:''},
+    {title:'Commissions', category:'Commissions', driveFolder:''}
+  ]
+};
